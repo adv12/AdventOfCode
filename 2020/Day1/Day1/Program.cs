4504b82ec5bfb85bd90accd1e7f33951b3b42c51
Andrew Vardeman <andrewdev@the-vardemans.net>
2020-12-01 11:48:23 -0600

    2020 Day 1

---
+++ 2020/Day1/Day1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+string[] lines = File.ReadAllLines(args[0]);
+var vals = new List<int>(lines.Length);
+bool twoValues = false;
+bool threeValues = false;
+foreach (string line in lines)
+{
+    vals.Add(int.Parse(line.Trim()));
+}
+for (int i = 0; i < vals.Count; i++)
+{
+    for (int j = 0; j < vals.Count; j++)
+    {
+        if (i != j && vals[i] + vals[j] == 2020)
+        {
+            Console.WriteLine("Two values: " + (vals[i] * vals[j]));
+            twoValues = true;
+            if (threeValues)
+            {
+                return;
+            }
+        }
+        if (!threeValues)
+        {
+            for (int k = 0; k < vals.Count; k++)
+            {
+                if (i != j && i != k && j != k && vals[i] + vals[j] + vals[k] == 2020)
+                {
+                    Console.WriteLine("Three values: " + vals[i] * vals[j] * vals[k]);
+                    threeValues = true;
+                    if (twoValues)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
+
