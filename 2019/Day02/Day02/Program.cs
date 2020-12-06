65c0a94c0ff7d46a87e32e56968a3e0a333a3cb8
Andrew Vardeman <andrewdev@the-vardemans.net>
2020-12-06 17:38:15 -0600

    2019 Days 2 and 3

---
+++ 2019/Day02/Day02/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+long RunProgram(long[] memory)
+{
+    long ip = 0;
+    while (true)
+    {
+        switch (memory[ip])
+        {
+            case 99:
+                return memory[0];
+            case 1:
+                memory[memory[ip + 3]] = memory[memory[ip + 1]] + memory[memory[ip + 2]];
+                ip += 4;
+                break;
+            case 2:
+                memory[memory[ip + 3]] = memory[memory[ip + 1]] * memory[memory[ip + 2]];
+                ip += 4;
+                break;
+        }
+    }
+}
+
+long PartOne(long[] program)
+{
+    var memory = (long[])program.Clone();
+
+    memory[1] = 12;
+    memory[2] = 2;
+
+    return RunProgram(memory);
+}
+
+long PartTwo(long[] program)
+{
+    for (int i = 0; i < 100; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            var memory = (long[])program.Clone();
+
+            memory[1] = i;
+            memory[2] = j;
+
+            if (RunProgram(memory) == 19690720)
+            {
+                return 100 * i + j;
+            }
+        }
+    }
+    return -1;
+}
+
+string text = File.ReadAllText("/Users/andrewvardeman/Desktop/AOC2019Input/02.txt").Trim();
+
+long[] program = text.Split(',').Select(s => long.Parse(s)).ToArray();
+
+Console.WriteLine(PartOne(program));
+
+Console.WriteLine(PartTwo(program));
---
+++ 2019/Day03/Day03/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+(HashSet<Point>, Dictionary<Point, int>) GetCoveredPointsAndStepCounts(string wire)
+{
+    HashSet<Point> points = new HashSet<Point>();
+    Dictionary<Point, int> stepCounts = new Dictionary<Point, int>();
+    string[] instructions = wire.Trim().Split(',');
+    int x = 0, y = 0;
+    int steps = 0;
+    foreach (string instruction in instructions)
+    {
+        int diff = int.Parse(instruction.Substring(1));
+        char dir = instruction[0];
+        int increment = 1;
+        if (dir == 'D' || dir == 'L')
+        {
+            increment = -1;
+        }
+        for (int i = 0; i < diff; i++)
+        {
+            if (dir == 'L' || dir == 'R')
+            {
+                x += increment;
+            }
+            else
+            {
+                y += increment;
+            }
+            Point p = new Point(x, y);
+            points.Add(p);
+            steps++;
+            if (!stepCounts.ContainsKey(p))
+            {
+                stepCounts[p] = steps;
+            }
+        }
+    }
+    return (points, stepCounts);
+}
+
+
+string[] wires = File.ReadAllLines("/Users/andrewvardeman/Desktop/AOC2019Input/03.txt");
+(var intersections, var stepCounts1) = GetCoveredPointsAndStepCounts(wires[0]);
+(var points2, var stepCounts2) = GetCoveredPointsAndStepCounts(wires[1]);
+intersections.IntersectWith(points2);
+Console.WriteLine($"Part 1: {intersections.Select(p => p.ManhattanDistance).OrderBy(d => d).First()}");
+Console.WriteLine($"Part 2: {intersections.Select(p => stepCounts1[p] + stepCounts2[p]).OrderBy(c => c).First()}");
+
+struct Point
+{
+    public int X;
+    public int Y;
+    public int ManhattanDistance => Math.Abs(X) + Math.Abs(Y);
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
