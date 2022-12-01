b1d8b01d46495d26f1cfb423d4f365424ce7d282
Andrew Vardeman <andrewdev@the-vardemans.net>
2022-12-01 13:16:06 -0600

    2022 Day 01

---
+++ 2022/Day01/Program.cs
@@ -0,0 +1,17 @@
+﻿var lines = File.ReadAllLines(args[0]);
+var sums = new int[lines.Length];
+int i = 0;
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        i++;
+    }
+    else
+    {
+        sums[i] += int.Parse(line);
+    }
+}
+Array.Sort(sums, (l, r) => r - l);
+Console.WriteLine(sums[0]);
+Console.WriteLine(sums[0] + sums[1] + sums[2]);
