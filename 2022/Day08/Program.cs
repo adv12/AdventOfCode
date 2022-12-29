4b681a4388bac0a018bd5b4f6e6caae3a9ccda33
Andrew Vardeman <andrewdev@the-vardemans.net>
2022-12-29 12:21:27 -0600

    2022 Days 08 and 09

---
+++ 2022/Day08/Program.cs
@@ -0,0 +1,114 @@
+﻿var lines = File.ReadAllLines(args[0]).Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+
+var numRows = lines.Length;
+var numCols = lines[0].Length;
+var heights = new byte[numRows, numCols];
+
+var visible = new bool[numRows, numCols];
+var visibleCount = 0;
+
+for (var i = 0; i < numRows; i++)
+{
+    var highest = -1;
+    for (var j = 0; j < numCols; j++)
+    {
+
+        heights[i, j] = byte.Parse(lines[i][j].ToString());
+        if (heights[i, j] > highest)
+        {
+            highest = heights[i, j];
+            visible[i, j] = true;
+        }
+    }
+    highest = -1;
+    for (var j = numCols - 1; j >= 0; j--)
+    {
+        if (heights[i, j] > highest)
+        {
+            highest = heights[i, j];
+            visible[i, j] = true;
+        }
+    }
+}
+
+for (var j = 0; j < numCols; j++)
+{
+    var highest = -1;
+    for (int i = 0; i < numRows; i++)
+    {
+        if (heights[i, j] > highest)
+        {
+            highest = heights[i, j];
+            visible[i, j] = true;
+        }
+    }
+    highest = -1;
+    for (var i = numRows - 1; i >= 0; i--)
+    {
+        if (heights[i, j] > highest)
+        {
+            highest = heights[i, j];
+            visible[i, j] = true;
+        }
+        if (visible[i, j])
+        {
+            visibleCount++;
+        }
+    }
+}
+
+Console.WriteLine(visibleCount);
+
+var highestScore = 0;
+for (var i = 1; i < numRows - 1; i++)
+{
+    for (var j = 1; j < numCols - 1; j++)
+    {
+        int height = heights[i, j];
+        var leftCount = 0;
+        var rightCount = 0;
+        var topCount = 0;
+        var bottomCount = 0;
+
+        for (var j2 = j - 1; j2 >= 0; j2--)
+        {
+            leftCount++;
+            if (heights[i, j2] >= height)
+            {
+                break;
+            }
+        }
+
+        for (var j2 = j + 1; j2 < numCols; j2++)
+        {
+            rightCount++;
+            if (heights[i, j2] >= height)
+            {
+                break;
+            }
+        }
+
+        for (var i2 = i - 1; i2 >= 0; i2--)
+        {
+            topCount++;
+            if (heights[i2, j] >= height)
+            {
+                break;
+            }
+        }
+
+        for (var i2 = i + 1; i2 < numRows; i2++)
+        {
+            bottomCount++;
+            if (heights[i2, j] >= height)
+            {
+                break;
+            }
+        }
+
+        var score = leftCount * rightCount * topCount * bottomCount;
+        highestScore = Math.Max(score, highestScore);
+    }
+}
+
+Console.WriteLine(highestScore);
---
+++ 2022/Day09/Program.cs
@@ -0,0 +1,99 @@
+﻿Tracker tracker = new Tracker(2);
+tracker.Run(args[0]);
+Console.WriteLine(tracker.TailLocationCount);
+
+tracker = new Tracker(10);
+tracker.Run(args[0]);
+//tracker.Print();
+Console.WriteLine(tracker.TailLocationCount);
+
+public enum Axis
+{
+    Horizontal,
+    Vertical
+}
+public class Tracker
+{
+    private int[] m_KnotXValues;
+    private int[] m_KnotYValues;
+    private HashSet<(int, int)> m_TailLocations = new HashSet<(int, int)>();
+    public int TailLocationCount => m_TailLocations.Count;
+    public Tracker(int numKnots)
+    {
+        m_KnotXValues = new int[numKnots];
+        m_KnotYValues = new int[numKnots];
+        m_TailLocations.Add((0, 0));
+    }
+    public void Run(string path)
+    {
+        var lines = File.ReadAllLines(path).Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+        foreach (string line in lines)
+        {
+            char direction = line[0];
+            int distance = int.Parse(line.Substring(2));
+            Axis axis = Axis.Horizontal;
+            if (direction == 'U' || direction == 'D')
+            {
+                axis = Axis.Vertical;
+            }
+            int sign = 1;
+            if (direction == 'D' || direction == 'L')
+            {
+                sign = -1;
+            }
+            MoveHead(axis, sign, distance);
+        }
+    }
+    public void MoveHead(Axis axis, int sign, int distance)
+    {
+        for (int i = 0; i < distance; i++)
+        {
+            switch(axis)
+            {
+                case Axis.Horizontal:
+                    m_KnotXValues[0] += sign;
+                    break;
+                case Axis.Vertical:
+                    m_KnotYValues[0] += sign;
+                    break;
+            }
+            MoveOtherKnots();
+        }
+    }
+    public void MoveOtherKnots()
+    {
+        for (int i = 1; i < m_KnotXValues.Length; i++)
+        {
+            int xDiff = m_KnotXValues[i - 1] - m_KnotXValues[i];
+            int yDiff = m_KnotYValues[i - 1] - m_KnotYValues[i];
+            if (Math.Abs(xDiff) > 1 || Math.Abs(yDiff) > 1)
+            {
+                m_KnotXValues[i] += Math.Sign(xDiff);
+                m_KnotYValues[i] += Math.Sign(yDiff);
+            }
+        }
+        m_TailLocations.Add((m_KnotXValues.Last(), m_KnotYValues.Last()));
+    }
+    public void Print()
+    {
+        int xmax = int.MinValue;
+        int xmin = int.MaxValue;
+        int ymax = int.MinValue;
+        int ymin = int.MaxValue;
+        foreach (var loc in m_TailLocations)
+        {
+            xmax = Math.Max(loc.Item1, xmax);
+            xmin = Math.Min(loc.Item1, xmin);
+            ymax = Math.Max(loc.Item2, ymax);
+            ymin = Math.Min(loc.Item2, ymin);
+        }
+        for (int y = ymax; y >= ymin; y--)
+        {
+            for (int x = xmin; x <= xmax; x++)
+            {
+                Console.Write(m_TailLocations.Contains((x, y)) ? "#" : ".");
+            }
+            Console.WriteLine();
+        }
+    }
+}
